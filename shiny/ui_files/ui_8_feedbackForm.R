32a86337556b6465a291adf5f77387d252bff94f
smwoodman <sam.woodman@noaa.gov>
2017-09-18 15:37:11 -0700

    Connected feedback form

--- shiny/ui_files/ui_8_feedbackForm.R
+++ shiny/ui_files/ui_8_feedbackForm.R
@@ -1,8 +1,19 @@
 ui.feedbackForm <- function() {
   tabItem(tabName = "feedbackForm",
           fluidRow(
-            box(title = NULL, width = 6, 
-                helpText("Feedback form has not yet been implemented")
+            box(
+              title = "Feedback Form", status = "warning", solidHeader = FALSE, width = 6, 
+              fluidRow(
+                column(6, textInput("feedback_name", h5("Your name"), value = "")), 
+                column(6, textInput("feedback_email", h5("Your email"), value = "")) 
+              ), 
+              selectInput("feedback_tab", h5("Tab relating to comment"), width = "50%", 
+                          choices = list("General" = 1, "Load Model Predictions" = 2, "Overlay Model Predictions" = 3, 
+                                         "Create Ensemble Predictions" = 4, "Evaluation Metrics" = 5, "High Quality Maps" = 6, 
+                                         "Export Predictions" = 7, "Manual" = 8), 
+                          selected = 1), 
+              textInput("feedback_comment", h5("Your comments. Please be as specific as possible"), value = ""), 
+              actionButton("feedback_submit", "Submit feedback")
             )
           )
   )
