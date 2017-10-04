85976614981898dc4d94bdeccf956c0029cc0d92
smwoodman <sam.woodman@noaa.gov>
2017-10-04 11:25:03 -0700

    check for internet reactive runs every 3 seconds

--- shiny/server_8_feedbackForm/server_8_feedbackForm.R
+++ shiny/server_8_feedbackForm/server_8_feedbackForm.R
@@ -3,9 +3,8 @@
 ###############################################################################
 ### Generate warning message if no internet connection is detected
 feedback_internet_connection <- reactive({
-  input$tabs
-  input$feedback_name
-  input$feedback_email
+  # Invalidate and thus rerun this reactive every 3 seconds
+  invalidateLater(millis = 3000, session)
   
   ifelse(isTruthy(try(getURL("www.google.com"), silent = TRUE)), 
          "",
@@ -13,42 +12,55 @@ feedback_internet_connection <- reactive({
                "You must be connected to the internet to submit feedback."))
 })
 
+
+###############################################################################
 ### Send feedback and give user confirmation message
 feedback_submit <- eventReactive(input$feedback_submit_event, {
-  ### Process user inputs to prepare for email
-  # User name and email
+  #####################################
+  ### Ensure that all field have an entry
+  list.in.feedback <- list(input$feedback_name, input$feedback_email, 
+                           input$feedback_tab, input$feedback_comment)
+  validate(
+    need(all(sapply(list.in.feedback, isTruthy)), 
+         "Please ensure that all fields have an entry")
+  )
+  
+  
+  #####################################
+  # Process user inputs to prepare for email
+  
+  ### User name and email
   f.name    <- paste("Name:", input$feedback_name)
   f.email   <- paste("Email:", input$feedback_email)
   
-  # Selected tab(s)
+  ### Selected tab(s)
   f.tab <- paste(ifelse(length(input$feedback_tab) > 1, "Tabs:", "Tab:"), 
                  paste(input$feedback_tab, collapse = "; "))
   
-  # Actual comment
-  f.comment <- unlist(strsplit(paste("Comments:",  input$feedback_comment), "\n"))
+  ### Actual comment
+  f.comment <- unlist(strsplit(paste("Comments:",  input$feedback_comment), 
+                               "\n"))
   
-  # Email details
-  from <- sprintf("<sendmailR@\\%s>", "eSDM")
-  to <- "<esdm.feedback@gmail.com>"
-  subject <- "eSDM feedback submission testing"
-  body <- list(f.name, f.email, f.tab, "", f.comment)
+  ### Email details
+  from    <- sprintf("<sendmailR@\\%s>", "eSDM")
+  to      <- "<esdm.feedback@gmail.com>"
+  subject <- paste0("eSDM feedback from ", input$feedback_name, "; ", 
+                    Sys.Date())
+  body    <- list(f.name, f.email, f.tab, "", f.comment)
   
-  # Check that all field have filled
-  validate(
-    need(all(sapply(body, isTruthy)), 
-         "Please provide an entry for all fields")
-  )
   
+  #####################################
   ### Send email
-  mail.out <- try(suppressWarnings(sendmail(from, to, subject, body,
-                                            control = list(smtpServer="ASPMX.L.GOOGLE.COM"))), 
-                  silent = TRUE)
+  mail.out <- try(suppressWarnings(
+    sendmail(from, to, subject, body, 
+             control = list(smtpServer="ASPMX.L.GOOGLE.COM"))), 
+    silent = TRUE)
   
   validate(
     need(isTruthy(mail.out), 
-         paste("Feedback could not be submitted; please check your internet", 
-               "connection. If this problem persists, contact", 
-               "Karin Forney (karin.forney@noaa.gov)."))
+         paste("Feedback could not be submitted; please check your", 
+               "internet connection. If this problem persists,", 
+               "contact Karin Forney (karin.forney@noaa.gov)."))
   )
   
   "Feedback submitted successfully. Thank you!"
