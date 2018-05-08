ab765101fbb9fe8487c606c6e1c406a466761e8d
smwoodman <sam.woodman@noaa.gov>
2018-05-08 09:04:23 -0700

    Fixed gshhg polys, f and h polys not in now because then package building takes a while

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' GSHHG world map
 #'
-#' Description.
+#' Description, intermediate resolution.
 #'
 #' @format A sfc object with class c("sfc_GEOMETRY", "sfc"):
 #' \describe{
@@ -11,3 +11,33 @@
 #' @source \url{http://www.soest.hawaii.edu/pwessel/gshhg/}
 
 "gshhg.i.L16"
+
+
+#' GSHHG world map
+#'
+#' Description, low resolution.
+#'
+#' @format A sfc object with class c("sfc_GEOMETRY", "sfc"):
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.soest.hawaii.edu/pwessel/gshhg/}
+
+"gshhg.l.L16"
+
+
+#' GSHHG world map
+#'
+#' Description, crude resolution.
+#'
+#' @format A sfc object with class c("sfc_GEOMETRY", "sfc"):
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.soest.hawaii.edu/pwessel/gshhg/}
+
+"gshhg.c.L16"
--- data-raw/GSHHG_shp.R
+++ data-raw/GSHHG_shp.R
@@ -1,23 +1,23 @@
 # Save app's default land files as .RDATA files
 # Shapefiles details: http://www.soest.hawaii.edu/pwessel/gshhg/
+# Didn't make sense to put GSHHG_shp folder in data-raw because it's so big
 
 library(sf)
 library(lwgeom)
 library(devtools)
 
 # Load L1 (world except for Antarctica) and L6 (Antarctica ground line)
-gshhg.f.L1 <- st_read("data-raw/GSHHG_shp/f/GSHHS_f_L1.shp")
-gshhg.f.L1 <- st_read("data-raw/GSHHG_shp/f/GSHHS_f_L1.shp")
-gshhg.h.L1 <- st_read("data-raw/GSHHG_shp/h/GSHHS_h_L1.shp")
-gshhg.i.L1 <- st_read("data-raw/GSHHG_shp/i/GSHHS_i_L1.shp")
-gshhg.l.L1 <- st_read("data-raw/GSHHG_shp/l/GSHHS_l_L1.shp")
-gshhg.c.L1 <- st_read("data-raw/GSHHG_shp/c/GSHHS_c_L1.shp")
+gshhg.f.L1 <- st_read("../Data files/GSHHG_shp/f/GSHHS_f_L1.shp")
+gshhg.h.L1 <- st_read("../Data files/GSHHG_shp/h/GSHHS_h_L1.shp")
+gshhg.i.L1 <- st_read("../Data files/GSHHG_shp/i/GSHHS_i_L1.shp")
+gshhg.l.L1 <- st_read("../Data files/GSHHG_shp/l/GSHHS_l_L1.shp")
+gshhg.c.L1 <- st_read("../Data files/GSHHG_shp/c/GSHHS_c_L1.shp")
 
-gshhg.f.L6 <- st_read("data-raw/GSHHG_shp/f/GSHHS_f_L6.shp")
-gshhg.h.L6 <- st_read("data-raw/GSHHG_shp/h/GSHHS_h_L6.shp")
-gshhg.i.L6 <- st_read("data-raw/GSHHG_shp/i/GSHHS_i_L6.shp")
-gshhg.l.L6 <- st_read("data-raw/GSHHG_shp/l/GSHHS_l_L6.shp")
-gshhg.c.L6 <- st_read("data-raw/GSHHG_shp/c/GSHHS_c_L6.shp")
+gshhg.f.L6 <- st_read("../Data files/GSHHG_shp/f/GSHHS_f_L6.shp")
+gshhg.h.L6 <- st_read("../Data files/GSHHG_shp/h/GSHHS_h_L6.shp")
+gshhg.i.L6 <- st_read("../Data files/GSHHG_shp/i/GSHHS_i_L6.shp")
+gshhg.l.L6 <- st_read("../Data files/GSHHG_shp/l/GSHHS_l_L6.shp")
+gshhg.c.L6 <- st_read("../Data files/GSHHG_shp/c/GSHHS_c_L6.shp")
 
 # Combine L1 and L6
 gshhg.f.L16 <- st_geometry(rbind(gshhg.f.L1, gshhg.f.L6))
@@ -42,12 +42,12 @@ gshhg.i.L16 <- st_make_valid(gshhg.i.L16)
 gshhg.l.L16 <- st_make_valid(gshhg.l.L16)
 gshhg.c.L16 <- st_make_valid(gshhg.c.L16)
 
-# # Check polygons validity again
-# all(st_is_valid(gshhg.f.L16))
-# all(st_is_valid(gshhg.h.L16))
-# all(st_is_valid(gshhg.i.L16))
-# all(st_is_valid(gshhg.l.L16))
-# all(st_is_valid(gshhg.c.L16))
+# Check polygons validity again
+all(st_is_valid(gshhg.f.L16))
+all(st_is_valid(gshhg.h.L16))
+all(st_is_valid(gshhg.i.L16))
+all(st_is_valid(gshhg.l.L16))
+all(st_is_valid(gshhg.c.L16))
 
 # Save to data folder
 devtools::use_data(gshhg.f.L16)
