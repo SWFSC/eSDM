bb4ea5a5aa22429a426e2345b30c2d85a358165d
smwoodman <sam.woodman@noaa.gov>
2018-06-13 17:53:06 -0700

    Updated provided data and fixed pretty plot download. Publishing to SMW's shinyapps.io

--- inst/shiny/server_5_prettyPlot/server_5_prettyPlot_download.R
+++ inst/shiny/server_5_prettyPlot/server_5_prettyPlot_download.R
@@ -5,32 +5,45 @@
 
 ### Download handler - High Quality Maps
 output$pretty_plot_download_execute <- downloadHandler(
-  filename = function() {
-    input$pretty_plot_download_name
-  },
-  
+  filename = function() input$pretty_plot_download_name,
+
   content = function(file) {
-    withProgress(message = "Downloading high quality map", value = 0.4, { 
-      plot.toprint <- pretty_plot_generate()
+    withProgress(message = "Downloading high quality map", value = 0.4, {
+      req(p.list <- vals$pretty.params.list)
       plot.res <- ifelse(input$pretty_plot_download_res == "1", 300, 72)
       pdf.res  <- ifelse(input$pretty_plot_download_res == "1", 15, 7)
       plot.format <- input$pretty_plot_download_format
       incProgress(0.2)
-      
+
       if(plot.format == 1) {
-        jpeg(file, width = 12, height = 12, units = 'in', res = plot.res, 
+        jpeg(file, width = 12, height = 12, units = 'in', res = plot.res,
              quality = 150)
-        grid.arrange(plot.toprint)
+        plot_pretty(
+          p.list$model.toplot, p.list$data.name, p.list$plot.lim, p.list$axes.inc,
+          p.list$title.ll, p.list$lab.x, p.list$lab.y,
+          p.list$title.cex, p.list$lab.cex, p.list$axis.cex, p.list$axis.tcl,
+          p.list$list.background, p.list$list.colorscheme, p.list$list.addobj
+        )
         dev.off()
       }
       if(plot.format == 2) {
         pdf(file, width = pdf.res, height = pdf.res)
-        grid.arrange(plot.toprint)
+        plot_pretty(
+          p.list$model.toplot, p.list$data.name, p.list$plot.lim, p.list$axes.inc,
+          p.list$title.ll, p.list$lab.x, p.list$lab.y,
+          p.list$title.cex, p.list$lab.cex, p.list$axis.cex, p.list$axis.tcl,
+          p.list$list.background, p.list$list.colorscheme, p.list$list.addobj
+        )
         dev.off()
       }
       if(plot.format == 3) {
         png(file, width = 12, height = 12, units = "in", res = plot.res)
-        grid.arrange(plot.toprint)
+        plot_pretty(
+          p.list$model.toplot, p.list$data.name, p.list$plot.lim, p.list$axes.inc,
+          p.list$title.ll, p.list$lab.x, p.list$lab.y,
+          p.list$title.cex, p.list$lab.cex, p.list$axis.cex, p.list$axis.tcl,
+          p.list$list.background, p.list$list.colorscheme, p.list$list.addobj
+        )
         dev.off()
       }
       incProgress(0.4)
