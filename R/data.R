eac20b656554d63e4b5a48ea0a4b19f8754e9ec6
smwoodman <sam.woodman@noaa.gov>
2018-06-27 18:25:30 -0700

    Moved all data other than low res GSHHG to eSDM data repo

--- R/data.R
+++ R/data.R
@@ -7,14 +7,3 @@
 #' @format An object with class c("sfc_GEOMETRY", "sfc"):
 #' @source \url{http://www.soest.hawaii.edu/pwessel/gshhg/}
 "gshhg.l.L16"
-
-
-#' Crude resolution GSHHG world map
-#'
-#' Crude resolution GSHHG world map, includes heirarchial levels
-#'   L1 and L6. Processed using lwgeom::st_make_valid() to make the
-#'   polygon valid
-#'
-#' @format An object with class c("sfc_GEOMETRY", "sfc"):
-#' @source \url{http://www.soest.hawaii.edu/pwessel/gshhg/}
-"gshhg.c.L16"
