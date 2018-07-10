79e9bff98b9d2e64e84c71f8f683501e8e34b829
smwoodman <sam.woodman@noaa.gov>
2018-07-10 16:47:08 -0700

    Fixed pretty plot par issue, pretty plot download, and pretty plot table single item selection

--- inst/shiny/server_5_prettyPlot/server_5_prettyPlot_download.R
+++ inst/shiny/server_5_prettyPlot/server_5_prettyPlot_download.R
@@ -9,7 +9,8 @@ output$pretty_plot_download_execute <- downloadHandler(
 
   content = function(file) {
     withProgress(message = "Downloading high quality map", value = 0.4, {
-      req(p.list <- vals$pretty.params.list)
+      req(p.list <- vals$pretty.plot.list)
+
       plot.res <- ifelse(input$pretty_plot_download_res == "1", 300, 72)
       pdf.res  <- ifelse(input$pretty_plot_download_res == "1", 15, 7)
       plot.format <- input$pretty_plot_download_format
@@ -18,32 +19,17 @@ output$pretty_plot_download_execute <- downloadHandler(
       if (plot.format == 1) {
         jpeg(file, width = 12, height = 12, units = 'in', res = plot.res,
              quality = 150)
-        plot_pretty(
-          p.list$model.toplot, p.list$data.name, p.list$plot.lim, p.list$axes.inc,
-          p.list$title.ll, p.list$lab.x, p.list$lab.y,
-          p.list$title.cex, p.list$lab.cex, p.list$axis.cex, p.list$axis.tcl,
-          p.list$list.background, p.list$list.colorscheme, p.list$list.addobj
-        )
+        plot_pretty_top(p.list$dims, p.list$idx.list, p.list$params.list)
         dev.off()
 
       } else if (plot.format == 2) {
         pdf(file, width = pdf.res, height = pdf.res)
-        plot_pretty(
-          p.list$model.toplot, p.list$data.name, p.list$plot.lim, p.list$axes.inc,
-          p.list$title.ll, p.list$lab.x, p.list$lab.y,
-          p.list$title.cex, p.list$lab.cex, p.list$axis.cex, p.list$axis.tcl,
-          p.list$list.background, p.list$list.colorscheme, p.list$list.addobj
-        )
+        plot_pretty_top(p.list$dims, p.list$idx.list, p.list$params.list)
         dev.off()
 
       } else if (plot.format == 3) {
         png(file, width = 12, height = 12, units = "in", res = plot.res)
-        plot_pretty(
-          p.list$model.toplot, p.list$data.name, p.list$plot.lim, p.list$axes.inc,
-          p.list$title.ll, p.list$lab.x, p.list$lab.y,
-          p.list$title.cex, p.list$lab.cex, p.list$axis.cex, p.list$axis.tcl,
-          p.list$list.background, p.list$list.colorscheme, p.list$list.addobj
-        )
+        plot_pretty_top(p.list$dims, p.list$idx.list, p.list$params.list)
         dev.off()
 
       } else {
