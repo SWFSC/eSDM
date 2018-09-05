b721016bac1239a55e2bc10170b3556d27a51d1f
smwoodman <sam.woodman@noaa.gov>
2018-09-05 13:40:35 -0700

    Renamed and finished documentation of pts_to_sfc functions

--- R/pts_to_sfc.R
+++ R/pts_to_sfc.R
@@ -1,11 +1,12 @@
-#' Create polygon from polygon coordinates
+#' Create polygons from coordinates of vertices
 #'
-#' Create object of class \code{sfc} of type \code{POLYGON} from a data frame with the polygon coordinates
+#' Create polygon(s) from a data frame with the coordinates of the polygon vertices
 #'
-#' @param x data frame with two columns;
-#'   the columns must contain longitude and latitude coordinates, respectively
+#' @param x data frame with at least two columns;
+#'   the first two columns must contain longitude and latitude coordinates, respectively.
+#'   All other columns are ignored
 #' @param crs.prov crs value for polygon being created;
-#'   value must be readable by \code{\link{sf}{st_crs}}
+#'   value must be readable by \code{\link[sf]{st_crs}}
 #
 #' @importFrom dplyr %>%
 #' @importFrom dplyr filter
@@ -18,22 +19,26 @@
 #' @importFrom sf st_polygon
 #' @importFrom sf st_sfc
 #'
-#' @details TODO
+#' @details Vertices of different polygons must be demarcated by rows with values of \code{NA}
+#'   in both the first and second columns (i.e. the longitude and latitude columns).
 #'
-#' @return Object of class \code{sfc} of type \code{POLYGON} with the provided coordinates and crs value
+#'   If \code{crs.prov} is not specified, then the \code{crs} attribute of the polygon(s) will be \code{NULL}.
+#'
+#' @return Object of class \code{sfc} of type \code{POLYGON} with the provided crs
 #'
 #' @examples
 #' x <- data.frame(
 #'   lon = c(40, 40, 50, 50, 40, NA, 20, 20, 30, 30, 20),
 #'   lat = c(0, 10, 10, 0, 0, NA, 0, 10, 10, 0, 0)
 #' )
-#' pts_to_sfc_coords(x, 4326)
+#' pts_to_sfc_vertices(x, 4326)
 #'
 #' @export
-pts_to_sfc_coords <- function(x, crs.prov = NULL) {
+pts_to_sfc_vertices <- function(x, crs.prov = NULL) {
   stopifnot(
     inherits(x, "data.frame"),
-    ncol(x) >= 2
+    ncol(x) >= 2,
+    identical(is.na(x[, 1]), is.na(x[, 2]))
   )
 
   x <- x %>%
@@ -65,39 +70,52 @@ pts_to_sfc_coords <- function(x, crs.prov = NULL) {
 }
 
 
-#' Create polygons from grid coordinates
+
+#' Create polygons from coordinates of centroids
 #'
-#' Create object of class \code{sfc} of type \code{POLYGON} from a data frame with
-#'   coordinates of the center of each polygon of a grid
+#' Create polygon(s) from a data frame with coordinates of the polygon centroid(s)
 #'
-#' @param x data frame with at least two columns;
-#'   the first two columns must contain longitude and latitude coordinates, respectively,
-#'   and the coordinates must form a grid
-#' @param poly.radius radius of each polygon to be created around points in \code{x};
-#'   must be numeric object
-#' @param crs.prov crs value of new sfc_polygon object;
-#'   value must be interpretable by st_crs(), see st_crs() documentation
+#' @inheritParams pts_to_sfc_vertices
+#' @param y numeric; shortest distance from centroid to any of the sides,
+#'   and thus also half the length of one side of the polygon
 #'
 #' @importFrom sf st_polygon
 #' @importFrom sf st_sfc
 #'
-#' @details TODO
+#' @details This function was designed for someone who reads in a .csv file
+#'   with a grid of coordinates representing SDM prediction points and needs to create
+#'   non-overlapping prediction polygons with the .csv file coordinates as the centroids.
+#'   However, it can be used to create square polygons around any points, even if those polygons overlap.
 #'
-#' @return Object of class \code{sfc} of type \code{POLYGON}
+#'   The created polygons are oriented so that, in a 2D plane, their edges are parallel to either the x or the y axis.
+#'
+#'   If \code{crs.prov} is not specified, then the \code{crs} attribute of the polygon(s) will be \code{NULL}.
+#'
+#' @return Object of class \code{sfc} of type \code{POLYGON} with the provided crs
 #'
 #' @examples
 #' x <- data.frame(
 #'   lon = c(5, 10, 15, 20, 5, 10, 15, 20),
 #'   lat = c(rep(5, 4), rep(10, 4))
 #' )
-#' pts_to_sfc_grid(x, 2.5, 4326)
+#' pts_to_sfc_centroids(x, 2.5, 4326)
+#'
+#' # Combine data and created sfc object to make sf object
+#' x <- data.frame(
+#'   lon = c(5, 10, 15, 20, 5, 10, 15, 20),
+#'   lat = c(5, 5, 5, 5, 10, 10, 10, 10),
+#'   sdm.pred = runif(8),
+#'   sdm.pred2 = runif(8)
+#' )
+#' x.sfc <- pts_to_sfc_centroids(x, 2.5, 4326)
+#' x.sf <- sf::st_sf(x[, 3:4], x.sfc, agr = "constant")
 #'
 #' @export
-pts_to_sfc_grid <- function(x, poly.radius, crs.prov = NULL) {
+pts_to_sfc_centroids <- function(x, y, crs.prov = NULL) {
   stopifnot(
     inherits(x, "data.frame"),
     ncol(x) >= 2,
-    inherits(poly.radius, "numeric")
+    inherits(y, "numeric") | inherits(y, "integer")
   )
 
   sfc.list <- apply(x, 1, function(i, j) {
@@ -107,7 +125,7 @@ pts_to_sfc_grid <- function(x, poly.radius, crs.prov = NULL) {
         i[2] + j, i[2] + j, i[2] - j, i[2] - j, i[2] + j),
       ncol = 2
     ))))
-  }, j = poly.radius)
+  }, j = y)
 
   if (is.null(crs.prov)) {
     st_sfc(do.call(rbind, sfc.list))
