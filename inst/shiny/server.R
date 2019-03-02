185615aedde3b036a53b6d65e05a0f9b664fb2c5
smwoodman <sam.woodman@noaa.gov>
2019-03-01 17:14:41 -0800

    Updated manual

--- inst/shiny/server.R
+++ inst/shiny/server.R
@@ -121,7 +121,7 @@ server <- function(input, output, session) {
   ### Create ensemble predictions
   source(file.path("server_3_createEns", "server_3_createEns.R"), local = TRUE, chdir = TRUE)
   source(file.path("server_3_createEns", "server_3_createEns_create.R"), local = TRUE, chdir = TRUE)
-  source(file.path("server_3_createEns", "server_3_createEns_create_regionalweighting.R"), local = TRUE, chdir = TRUE)
+  source(file.path("server_3_createEns", "server_3_createEns_create_regexc.R"), local = TRUE, chdir = TRUE)
   source(file.path("server_3_createEns", "server_3_createEns_create_weighted.R"), local = TRUE, chdir = TRUE)
   source(file.path("server_3_createEns", "server_3_createEns_renderUI.R"), local = TRUE, chdir = TRUE)
   source(file.path("server_3_createEns", "server_3_createEns_variance.R"), local = TRUE, chdir = TRUE)
