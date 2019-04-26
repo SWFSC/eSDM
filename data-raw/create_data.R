40d9fedf50193c775a2e9f6c94c10dc868393549
smwoodman <sam.woodman@noaa.gov>
2019-04-26 15:57:16 -0700

    Updated eSDM package provided data

--- data-raw/create_data.R
+++ data-raw/create_data.R
@@ -1,3 +1,4 @@
+library(dplyr)
 library(eSDM)
 library(sf)
 
@@ -11,7 +12,7 @@ z <- st_transform(
   st_read("data-raw/eSDM_shpExport (2)/eSDM_Sample_predictions_4_gdb__Pred1__orig.shp", agr = "constant"),
   4326
 )
-bite <- eSDM::pts_to_sfc_coords(read.csv("../data_provided/SoCal_bite.csv"), 4326)
+bite <- eSDM::pts2poly_vertices(read.csv("../eSDM-data/Ignore/eSDM_data_sample/SoCal_bight.csv"), crs = 4326)
 
 preds.1 <- st_set_agr(st_crop(x, bite), "constant")
 preds.2 <- st_set_agr(st_crop(y, bite), "constant")
@@ -23,31 +24,31 @@ row.names(preds.2) <- 1:nrow(preds.2)
 row.names(preds.3) <- 1:nrow(preds.3)
 
 # Spice up preds.1 for the sake of examples
-preds.1 <- st_sf(
-  data.frame(st_set_geometry(preds.1, NULL), Density2 = runif(325)),
-  geometry = st_geometry(preds.1), agr = "constant"
-)
+preds.1 <-  data.frame(st_set_geometry(preds.1, NULL)) %>%
+  mutate(Density2 = runif(325),
+         Var1 = runif(325) / 100,
+         Var2 = runif(325) / 100) %>%
+  st_sf(geometry = st_geometry(preds.1), agr = "constant")
 
 # plot(bite, axes = TRUE)
 # plot(st_geometry(preds.1), add = TRUE, col = NA, border = "red")
 # plot(st_geometry(preds.2), add = TRUE, col = NA, border = "blue")
 # plot(st_geometry(preds.3), add = TRUE, col = NA, border = "green")
 
-devtools::use_data(preds.1)
-devtools::use_data(preds.2)
-devtools::use_data(preds.3)
+usethis::use_data(preds.1) #, overwrite = TRUE)
+usethis::use_data(preds.2)
+usethis::use_data(preds.3)
 
 
 ### Create sample validation data to use in examples
-x <- read.csv("../data_provided/Validation_data.csv")
-validation.data <- st_as_sf(
-  x[, c("mlon", "mlat", "sight", "count")], crs = 4326,
-  coords = c("mlon","mlat"), agr = "constant"
-)
+x <- read.csv("../eSDM-data/Ignore/eSDM_data_sample/Validation_data.csv")
+validation.data <- x %>%
+  select(lon, lat, sight, count) %>%
+  st_as_sf(coords = c("lon","lat"), crs = 4326, agr = "constant")
 validation.data <- st_crop(validation.data, bite)
 
 # plot(bite, axes = TRUE)
 # plot(st_geometry(validation.data), add = TRUE)
 # plot(validation.data[2], pch = 19)
 
-devtools::use_data(validation.data)
+usethis::use_data(validation.data)
