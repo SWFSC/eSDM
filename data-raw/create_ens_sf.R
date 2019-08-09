684c26765bcc6d365bee2b7f027a7ade0ae73652
Sam Woodman <swoodman213@gmail.com>
2019-08-09 10:55:33 -0700

    data-raw: added GUi environment from example analysis processing, explicit script for creating rdas for manuscript images

---
+++ data-raw/create_ens_sf.R
@@ -0,0 +1,26 @@
+# Create ens_sf_...rda objects to use in scripts that create ME&E paper figs
+# Loaded .RDATA file is saved GUI environment when using data from
+#   eSDM_data_manuscript.zip
+
+library(dplyr)
+library(sf)
+
+# Load saved GUI environment
+load("data-raw/eSDM_ME&E_final.RDATA")
+
+# Check that ensembles are at expected indices
+stopifnot(
+  vals.save$ensemble.specs[[1]]["ensmethod"] == "Unweighted",
+  vals.save$ensemble.specs[[3]]["ensmethod"] == "Weighted - TSS-based"
+)
+
+# Unweighted ensemble
+ens.sf.unw <- vals.save$ensemble.models[[1]] %>%
+  mutate(CV_ens = SE_ens / Pred_ens) %>%
+  st_sf(geometry = vals.save$overlay.base.sfc,agr = "constant")
+save(ens.sf.unw, file = "data-raw/ens_sf_unw.rda")
+
+# Ensemble weighted by TSS
+ens.sf.wtss <- vals.save$ensemble.models[[3]] %>%
+  st_sf(geometry = vals.save$overlay.base.sfc,agr = "constant")
+save(ens.sf.wtss, file = "data-raw/ens_sf_wtss.rda")
