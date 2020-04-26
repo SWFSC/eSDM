f3398cf827800b93adc8d6ba27657beefc61afdb
Sam Woodman <swoodman213@gmail.com>
2020-04-25 18:40:36 -0700

    Copy sf travis

--- tic.R
+++ tic.R
@@ -1,8 +1,19 @@
-# installs dependencies, runs R CMD check, runs covr::codecov()
+# # installs dependencies, runs R CMD check, runs covr::codecov()
+# do_package_checks()
+#
+# if (ci_on_travis() && ci_has_env("BUILD_PKGDOWN")) {
+#   # creates pkgdown site and pushes to gh-pages branch
+#   # only for the runner with the "BUILD_PKGDOWN" env var set
+#   do_pkgdown()
+# }
+
 do_package_checks()
 
-if (ci_on_travis() && ci_has_env("BUILD_PKGDOWN")) {
-  # creates pkgdown site and pushes to gh-pages branch
-  # only for the runner with the "BUILD_PKGDOWN" env var set
-  do_pkgdown()
-}
+get_stage("install") %>%
+  # install lwgeom with its own library since linking again postgis source install fails sometimes
+  add_code_step(install.packages("lwgeom", configure.args="--without-liblwgeom"))
+
+###
+# deploy pkgdowm site
+###
+do_pkgdown(document = FALSE)
