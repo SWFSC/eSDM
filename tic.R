42670ba9ba21d53d8861dcb0e36ae454c98eb728
Sam Woodman <swoodman213@gmail.com>
2020-04-25 19:03:51 -0700

    travis clean2

--- tic.R
+++ tic.R
@@ -9,9 +9,9 @@
 
 do_package_checks()
 
-get_stage("install") %>%
-  # install lwgeom with its own library since linking again postgis source install fails sometimes
-  add_code_step(install.packages("lwgeom", configure.args="--without-liblwgeom"))
+# get_stage("install") %>%
+#   # install lwgeom with its own library since linking again postgis source install fails sometimes
+#   add_code_step(install.packages("lwgeom", configure.args="--without-liblwgeom"))
 
 ###
 # deploy pkgdowm site
