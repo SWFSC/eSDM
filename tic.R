3b1b44dc78e6850aa97bd9c8a448044bb1aa6a93
Sam Woodman <swoodman213@gmail.com>
2020-04-25 19:41:01 -0700

    Bump required R version b/c of caTools req. Travis and tic cleanup

--- tic.R
+++ tic.R
@@ -1,13 +1,4 @@
-# # installs dependencies, runs R CMD check, runs covr::codecov()
-# do_package_checks()
-#
-# if (ci_on_travis() && ci_has_env("BUILD_PKGDOWN")) {
-#   # creates pkgdown site and pushes to gh-pages branch
-#   # only for the runner with the "BUILD_PKGDOWN" env var set
-#   do_pkgdown()
-# }
-
+# Copied over from sf/.tic.R, and then pared down as possible
 do_package_checks()
-
 
 do_pkgdown()
