5aeeb019d883465d3c8d196f9c97cfbd8db506dd
Sam Woodman <swoodman213@gmail.com>
2020-09-04 17:37:23 -0700

    hopefully travis fix

--- tic.R
+++ tic.R
@@ -1,4 +1,8 @@
 # Copied over from sf/.tic.R, and then pared down as possible
 do_package_checks()
 
-do_pkgdown()
+if (ci_on_travis() && ci_has_env("BUILD_PKGDOWN")) {
+  # creates pkgdown site and pushes to gh-pages branch
+  # only for the runner with the "BUILD_PKGDOWN" env var set
+  do_pkgdown()
+}
