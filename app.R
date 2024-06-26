21b49a0db76ad5c3e5d3a9ee6c69128e9c02c385
Sam Woodman - NOAA <sam.woodman@noaa.gov>
2024-06-25 17:45:36 -0700

    Add file for deploying Shiny app

---
+++ app.R
@@ -0,0 +1,6 @@
+# Launch the ShinyApp (Do not remove this comment)
+# To deploy, run: rsconnect::deployApp()
+# Or use the blue button on top of this file
+pkgload::load_all(export_all = T, helpers = FALSE, attach_testthat = FALSE)
+
+eSDM::eSDM_GUI()
